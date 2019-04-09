b3f72ee04b9d8f945639359c68705df935ca1798
trhtran <htrantr@gmail.com>
2019-04-09 23:49:50 +0200

    fit using stationary

--- timeseriesAnalysis.R
+++ timeseriesAnalysis.R
@@ -1,64 +1,84 @@
 
 library(ggplot2)
-theme_set(theme_minimal())
+library(lmtest)
+library("fUnitRoots")
+library(FitAR)
+library(forecast)
+library(Metrics)
+
+
+#theme_set(theme_minimal())
 
 
 #library(xts)
 
 MyData <- read.csv(file="timeseries.txt", header=TRUE, sep=",")
-
-#MyData_ts <- xts(MyData$PP, order.by = as.POSIXct(MyData$date))
-
-#plot(MyData_ts, main = "PP", xlab = "date", ylab = "$", col="blue")
-#components.ts = decompose(MyData_ts)
+nrow(MyData)
+train <- MyData[1:84,]
+valid <- MyData[85:nrow(MyData),]
 
 
+train_ts <- ts(train$PP, start=c(2011,01), frequency=12)
+valid_ts <- ts(valid$PP, start=c(2018,01), frequency=12)
 
-myts <- ts(MyData$PP, start=c(2011,01), frequency=12)
+timeseriescomponents = decompose(train_ts)
 
-timeseriescomponents = decompose(myts)
-plot(components.ts)
 plot(timeseriescomponents)
 
-library("fUnitRoots")
+
 
 # https://datascienceplus.com/time-series-analysis-using-arima-model-in-r/
-  
+# https://github.com/SubhasreeUC/Master/blob/master/TimeSeriesExample.R  
 
-urkpssTest(myts, type = c("tau"), lags = c("short"),use.lag = NULL, doplot = TRUE)
-tsstationary = diff(myts, differences=1)
+urkpssTest(train_ts, type = c("tau"), lags = c("short"),use.lag = NULL, doplot = TRUE)
+tsstationary = diff(train_ts, differences=1)
 plot(tsstationary)
 
-acf(MyData_ts,lag.max=34) 
+acf(train_ts,lag.max=24) 
 
 
-timeseriesseasonallyadjusted <- myts - timeseriescomponents$seasonal
+timeseriesseasonallyadjusted <- train_ts - timeseriescomponents$seasonal
 tsstationary <- diff(timeseriesseasonallyadjusted, differences=1)
 
 
-acf(tsstationary, lag.max=34)
-pacf(tsstationary, lag.max=34)
+acf(tsstationary, lag.max=24)
+pacf(tsstationary, lag.max=24)
 
 
-fitARIMA <- arima(myts, order=c(1,1,1),seasonal = list(order = c(1,0,0), period = 12),method="ML")
-library(lmtest)
+#fitARIMA <- arima(train_ts, order=c(1,1,1),seasonal = list(order = c(1,0,0), period = 12),method="ML")
+fitARIMA <- arima(tsstationary, order=c(1,1,1),seasonal = list(order = c(1,0,0), period = 12),method="CSS")
+
+#fitARIMA <- arima(train_ts, order = c(0,1,1))
+
+
 coeftest(fitARIMA) 
 
 
 acf(fitARIMA$residuals)
-library(FitAR)
-boxresult-LjungBoxTest (fitARIMA$residuals,k=2,StartLag=1)
+
+boxresult <- LjungBoxTest (fitARIMA$residuals,k=2,StartLag=1)
 plot(boxresult[,3],main= "Ljung-Box Q Test", ylab= "P-values", xlab= "Lag")
 qqnorm(fitARIMA$residuals)
 qqline(fitARIMA$residuals)
 
 
+prediction <- predict(fitARIMA,12)
+
+prediction
+
+model = auto.arima(tsstationary, trace=TRUE) 
+#fitARIMA <- auto.arima(myts,max.p = 5,max.q = 5,max.P = 5,max.Q = 5,max.d = 3,seasonal = TRUE,ic = 'aicc')
+
+
+
+
+
+
 # Forecast
-auto.arima(myts, trace=TRUE) 
 
-predict(fitARIMA,n.ahead = 12)
-
-library(forecast)
-
-futurVal <- forecast(fitARIMA,h=10, level=c(99.5))
+futurVal <- forecast(model,h=12, level=c(99.5))
 plot(futurVal)
+plot(valid)
+
+rmse(valid$PP, prediction$pred)
+
